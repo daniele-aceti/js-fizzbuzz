447f9f3859a0061c8cd66d9399c58f45def0e9ee
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-11 13:41:03 +0100

    feat: soluzione

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 for (let i = 1; i<= 100; i++){
+    let x = (i % 3)
+    let y = (i % 5)
+    if(!x && !y){
+        console.log(`FizzBuzz`)
+    }else if(!x){
+        console.log(`Fiz`)
+    }else if (!y){
+        console.log(`Buzz`)
+    }else{
+        console.log(i)
+    }
+}
+
+
+/*oppure
+
+
+for (let i = 1; i<= 100; i++){
     let x = (i % 3)=== 0
     let y = (i % 5)=== 0
     if(x && y){
@@ -10,4 +28,4 @@ for (let i = 1; i<= 100; i++){
     }else{
         console.log(i)
     }
-}
+}*/
