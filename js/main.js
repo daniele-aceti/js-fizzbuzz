ca7f4e326c25a59b34087272dc9e35d85ffb2f85
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-11 12:40:45 +0100

    feat: FizzBuzz

---
+++ js/main.js
@@ -0,0 +1,13 @@
+for (let i = 1; i<= 100; i++){
+    let x = (i % 3)=== 0
+    let y = (i % 5)=== 0
+    if(x && y){
+        console.log(`FizzBuzz`)
+    }else if(x){
+        console.log(`Fiz`)
+    }else if (y){
+        console.log(`Buzz`)
+    }else{
+        console.log(i)
+    }
+}
