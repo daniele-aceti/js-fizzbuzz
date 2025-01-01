dd357bc74724a974257a5d2721a35563da8f24c5
daniele-aceti <daniele.aceti@hotmail.it>
2025-01-01 16:31:03 +0100

    feat: inserita soluzione meno leggibile tra le note

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 for (let i = 1; i<= 100; i++){
-    let mod3 = (i % 3)
-    let mod5 = (i % 5)
-    if(!mod3 && !mod5){
-        console.log(`FizzBuzz`)
-    }else if(!mod3){
-        console.log(`Fiz`)
-    }else if (!mod5){
-        console.log(`Buzz`)
-    }else{
-        console.log(i)
-    }
-}
-
-
-/*oppure
-
-
-for (let i = 1; i<= 100; i++){
     let mod3 = (i % 3) === 0
     let mod5 = (i % 5) === 0
     if(mod3 && mod5){
@@ -30,4 +12,18 @@ for (let i = 1; i<= 100; i++){
     }
 }
     
-*/
+/*oppure
+
+for (let i = 1; i<= 100; i++){
+    let mod3 = (i % 3)
+    let mod5 = (i % 5)
+    if(!mod3 && !mod5){
+        console.log(`FizzBuzz`)
+    }else if(!mod3){
+        console.log(`Fiz`)
+    }else if (!mod5){
+        console.log(`Buzz`)
+    }else{
+        console.log(i)
+    }
+}*/
